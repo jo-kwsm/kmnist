f2e19d9ba31c37e104576f59db98530deff28077
jo-kwsm <ytk.kwsm@gmail.com>
2020-11-26 23:32:16 +0900

    vggとinceptionに対応

--- src/libs/models/__init__.py
+++ src/libs/models/__init__.py
@@ -3,7 +3,7 @@ import torchvision
 
 __all__ = ["get_model"]
 
-model_names = ["resnet18", "resnet34", "resnet50"]
+model_names = ["resnet18", "resnet34", "resnet50", "vgg16", "inception_v3"]
 
 
 def get_model(name: str, n_classes: int, pretrained: bool = True) -> nn.Module:
@@ -17,8 +17,16 @@ def get_model(name: str, n_classes: int, pretrained: bool = True) -> nn.Module:
 
     print("{} will be used as a model.".format(name))
     model = getattr(torchvision.models, name)(pretrained=pretrained)
-    model.conv1=nn.Conv2d(1, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
-    in_features = model.fc.in_features
-    model.fc = nn.Linear(in_features=in_features, out_features=n_classes, bias=True)
+    if name.startswith("resnet"):
+        model.conv1=nn.Conv2d(1, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
+        in_features = model.fc.in_features
+        model.fc = nn.Linear(in_features=in_features, out_features=n_classes, bias=True)
+    elif name.startswith("vgg"):
+        in_features = model.classifier[-1].in_features
+        model.classifier[-1] = nn.Linear(in_features=in_features, out_features=n_classes, bias=True)
+    elif name.startswith("inception"):
+        in_features = model.fc.in_features
+        model.fc = nn.Linear(in_features=in_features, out_features=n_classes, bias=True)
+    
 
     return model
