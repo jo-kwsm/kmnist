8a8acd331bef67ae4a6c60ab8bdd500cb4d83c97
jo-kwsm <ytk.kwsm@gmail.com>
2020-12-03 23:59:00 +0900

    kmnistデータセットに存在するモデルを追加

--- src/libs/models/net.py
+++ src/libs/models/net.py
@@ -1,6 +1,32 @@
 import torch.nn as nn
 from torch import flatten
 
+
+class BenchmarkNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=(3, 3), stride=(1, 1))
+        self.conv2 = nn.Conv2d(32, 64, ernel_size=(3, 3))
+        self.dropout1 = nn.Dropout(p=0.25)
+        self.fc1 = nn.Linear(in_features=128, out_features=128, bias=True)
+        self.dropout2 = nn.Dropout(p=0.5)
+        self.fc2 = nn.Linear(in_features=128, out_features=10, bias=True)
+        self.maxpool = nn.MaxPool2d((2, 2))
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        x = self.relu(self.conv1(x))
+        x = self.relu(self.conv2(x))
+        x = self.maxpool(x)
+        x = self.dropout1(x)
+        x = flatten(x, 1)
+        x = self.relu(self.fc1(x))
+        x = self.dropout2(x)
+        x = self.fc2(x)
+
+        return x
+
+
 class EasyNet(nn.Module):
     def __init__(self):
         super().__init__()
