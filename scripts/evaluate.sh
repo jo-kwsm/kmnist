a7aa3ef73df61ea192e28d7f5723c486582b13d7
jo-kwsm <ytk.kwsm@gmail.com>
2020-12-14 06:26:13 +0900

    評価用スクリプトの修正

--- scripts/evaluate.sh
+++ scripts/evaluate.sh
@@ -1,6 +1,7 @@
 files="./result/*"
 for filepath in $files; do
     if [ -d $filepath ] ; then
+        python src/evaluate.py "${filepath}/config.yaml" validation
         python src/evaluate.py "${filepath}/config.yaml" test
         python src/libs/graph.py "${filepath}/log.csv"
     fi
